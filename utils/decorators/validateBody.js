d6590953ce1f8577113270695264c40f85ad46d4
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-12-04 20:32:38 +0100

    fix validateBody/authenticate

--- utils/decorators/validateBody.js
+++ utils/decorators/validateBody.js
@@ -1,13 +1,11 @@
 import { HttpError } from "../helpers/HttpError.js";
 
 export const validateBody = (schema) => {
-  const func = (req, res, next) => {
+  return (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
       return next(HttpError(400, error.message));
     }
     next();
   };
-
-  return func;
 };
--- utils/middlewares/authenticate.js
+++ utils/middlewares/authenticate.js
@@ -11,7 +11,7 @@ const authenticate = async (req, res, next) => {
     return next(HttpError(401, "Authorization header not found"));
   }
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     return next(HttpError(401, "Not authorized"));
   }
   try {
